c1adb250270980a95651dbdd4eb2d7ef19767875
Sophia Dolan <25srdolan@gmail.com>
2025-02-11 16:33:28 -0500

    task 3

--- cc_example.js
+++ cc_example.js
@@ -22,3 +22,13 @@ function applydiscount (products, discountRate){
 console.log(applydiscount(products, .1))
 console.log("*".repeat(20))
 console.log(products);
+
+//task 3
+
+let sales = [250, 400, 150, 900, 1200];
+
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale) => total + sale, 0);
+}
+
+console.log(`Total Rev.2024: $${calculateTotalRevenue(sales)}`);
